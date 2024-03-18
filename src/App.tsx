ab136369a7bac7f146d5d115af840038232be59f
Ali Athary <ali.athary.ce@gmail.com>
2024-03-18 11:31:15 +0330

    setup project and chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box>
+      <Text>Project setup</Text>
+      <Button colorScheme="blue">Button</Button>
+    </Box>
+  );
+}
+
+export default App;
